5046fb2eb8f1663eb76e19fd6a73c75bde7a3204
shirley nguyen <shirleynguyen@berkeley.edu>
2020-06-26 00:57:36 -0700

    Version where MyCardForm is a ModelForm

--- amiispace_app/forms.py
+++ amiispace_app/forms.py
@@ -7,43 +7,27 @@ class MyCardForm(forms.ModelForm):
 	
 	class Meta:
 		model = MyCard
-		exclude = ('owner', 
-			'card',
-			'quality',
-			'bought_price',
-			'selling_price')
+		exclude = ['owner', 'card', 'quality', 'bought_price', 'selling_price']
 
-	def to_python(self, value):
-		""" Normalize data to a list of strings. """
-		print("to_python")
-		if not value:
-			return []
-		print(value.split())
-		return value.split()
-
-	def validate(self, value):
-		""" Check if value consists only of valid cards. """
-		print("validate")
-		super().validate(value)
-		for card in value:
-			print(card)
-			validate_card(card)
-
-	def validate_card(self, card):
-		""" Check if card is a valid card. 
-			ex. 170 NA """
-		print("validate_card")
-		formatted = card.split()
-		l = len(formatted)
-		
-		if l != 2:
-			raise forms.ValidationError("{0} is not a valid input.".format(card))
-
-		number = formatted[0]
-		version = formatted[1]
-		valid = Card.objects.get(number=number, version=version)
-
-		if valid:
-			raise forms.ValidationError("{0} is not a valid card.".format(card))
-		else:
-			return
+	def clean_bulk_field(self):
+		""" Checks if cards are formatted correctly and exist in the catalogue. 
+			ex. 170 NA
+				Ruby NA
+			Returns list of formatted cards. """
+		cards = self.cleaned_data['bulk_field'].split('\r\n')
+		formatted_cards = [card.strip() for card in cards]
+		for card in formatted_cards:
+			data = card.split()
+			if len(data) != 2:
+				raise forms.ValidationError("{0} is not a valid format.".format(card))
+			misc = data[0]
+			version = data[1]
+			exists = None
+			if misc.isalpha():
+				exists = Card.objects.filter(character=misc, version=version)
+			else:
+				exists = Card.objects.filter(number=misc, version=version)
+			if not exists:
+				raise forms.ValidationError("{0} is not a valid card.".format(card))
+		print(formatted_cards)
+		return formatted_cards
--- amiispace_app/views.py
+++ amiispace_app/views.py
@@ -4,12 +4,14 @@ from django.http import HttpResponse
 from amiispace_app.forms import MyCardForm
 
 def index(request):
-	form = MyCardForm(request.POST or None)
-	print(form.is_valid())
-	print(form.errors)
-	if form.is_valid():
-		return redirect('/amiispace_app/index')
-	else:
+	context = {}
+	if request.method == "POST":
 		form = MyCardForm(request.POST)
-	context = {'form': form}
+		if form.is_valid():
+			return redirect('/amiispace_app/index')
+		else:
+			context['form'] = form
+			render(request, 'amiispace_app/index.html', context)
+	else:
+		context['form'] = MyCardForm()
 	return render(request, 'amiispace_app/index.html', context)

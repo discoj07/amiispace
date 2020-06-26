0964345756943828a5414039580eda2eda5710bc
shirley nguyen <shirleynguyen@berkeley.edu>
2020-06-26 01:32:50 -0700

    Bulk field successfully processes data

--- amiispace_app/forms.py
+++ amiispace_app/forms.py
@@ -13,10 +13,12 @@ class MyCardForm(forms.ModelForm):
 		""" Checks if cards are formatted correctly and exist in the catalogue. 
 			ex. 170 NA
 				Ruby NA
-			Returns list of formatted cards. """
+			Returns list of tuples i.e. [(Ruby, NA), (Kabuki, EU)]. """
 		cards = self.cleaned_data['bulk_field'].split('\r\n')
 		formatted_cards = [card.strip() for card in cards]
-		for card in formatted_cards:
+
+		for i in range(len(formatted_cards)):
+			card = formatted_cards[i]
 			data = card.split()
 			if len(data) != 2:
 				raise forms.ValidationError("{0} is not a valid format.".format(card))
@@ -27,7 +29,10 @@ class MyCardForm(forms.ModelForm):
 				exists = Card.objects.filter(character=misc, version=version)
 			else:
 				exists = Card.objects.filter(number=misc, version=version)
+
 			if not exists:
 				raise forms.ValidationError("{0} is not a valid card.".format(card))
-		print(formatted_cards)
+
+			formatted_cards[i] = (exists[0].character, version)
+
 		return formatted_cards
--- amiispace_app/views.py
+++ amiispace_app/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from amiispace_app.forms import MyCardForm
+from amiispace_app.models import MyCard, Card
 
 def index(request):
 	context = {}
 	if request.method == "POST":
 		form = MyCardForm(request.POST)
 		if form.is_valid():
+			for tup in form.cleaned_data['bulk_field']:
+				character = tup[0]
+				version = tup[1]
+				query = Card.objects.filter(character=character, version=version)
+				card = query[0]
+				print(card)
+				# create MyCard object but need user
 			return redirect('/amiispace_app/index')
 		else:
 			context['form'] = form

f1df04403e23022f3d32ef5b1cfc46f8906acd80
shirley nguyen <shirleynguyen@berkeley.edu>
2020-06-27 01:48:29 -0700

    Separated into MyCardForm and NewCardForm

--- amiispace_app/forms.py
+++ amiispace_app/forms.py
@@ -3,36 +3,41 @@ from django import forms
 from .models import Card, MyCard
 
 class MyCardForm(forms.ModelForm):
-	bulk_field = forms.CharField(widget=forms.Textarea, required=True)
-	
 	class Meta:
 		model = MyCard
-		exclude = ['owner', 'card', 'quality', 'bought_price', 'selling_price']
+		exclude = ['owner', 'card']
+
+class NewCardForm(forms.Form):
+	bulk_field = forms.CharField(widget=forms.Textarea, required=True)
 
 	def clean_bulk_field(self):
-		""" Checks if cards are formatted correctly and exist in the catalogue. 
+		""" Checks if cards are formatted correctly and exist in the catalogue.
 			ex. 170 NA
 				Ruby NA
-			Returns list of tuples i.e. [(Ruby, NA), (Kabuki, EU)]. """
-		cards = self.cleaned_data['bulk_field'].split('\r\n')
-		formatted_cards = [card.strip() for card in cards]
+			Returns list of tuples i.e. [('Fang', 'NA'), ('Kabuki', 'EU')] """
+		data = self.cleaned_data['bulk_field']
+		cards = data.split('\r\n')
+		fcards = [card.strip() for card in cards]
 
-		for i in range(len(formatted_cards)):
-			card = formatted_cards[i]
-			data = card.split()
-			if len(data) != 2:
-				raise forms.ValidationError("{0} is not a valid format.".format(card))
-			misc = data[0]
-			version = data[1]
+		for i in range(len(fcards)):
+			fcard = fcards[i]
+			info = fcard.split()
+			
+			if len(info) != 2:
+				raise forms.ValidationError("{0} is not a valid format.".format(fcard))
+			
+			misc = info[0]
+			version = info[1]
 			exists = None
+
 			if misc.isalpha():
 				exists = Card.objects.filter(character=misc, version=version)
 			else:
 				exists = Card.objects.filter(number=misc, version=version)
 
 			if not exists:
-				raise forms.ValidationError("{0} is not a valid card.".format(card))
+				raise forms.ValidationError("{0} is not a valid card.".format(fcard))
 
-			formatted_cards[i] = (exists[0].character, version)
+			fcards[i] = (exists.first().character, version)
 
-		return formatted_cards
+		return fcards
--- amiispace_app/views.py
+++ amiispace_app/views.py
@@ -1,29 +1,28 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model 
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 
-from amiispace_app.forms import MyCardForm
+from amiispace_app.forms import MyCardForm, NewCardForm
 from amiispace_app.models import MyCard, Card
 
 def index(request):
+	""" Add cards view. """
 	context = {}
-	if request.method == "POST":
-		form = MyCardForm(request.POST)
-		print(request.user)
+	if request.method == 'POST':
+		form = NewCardForm(request.POST)
 		if form.is_valid():
-			for tup in form.cleaned_data['bulk_field']:
-				character = tup[0]
-				version = tup[1]
+			user = get_user_model().objects.first()
+			cards = form.cleaned_data['bulk_field']
+			for card in cards:
+				character = card[0]
+				version = card[1]
 				query = Card.objects.filter(character=character, version=version)
-				card = query[0]
-				user = get_user_model().objects.first()
-				my_card = MyCard(owner=user, card=card)
+				my_card = MyCard(owner=user, card = query.first())
 				my_card.save()
-			return redirect('/amiispace_app/index')
+				return redirect('/amiispace_app/index')
 		else:
 			context['form'] = form
-			render(request, 'amiispace_app/index.html', context)
+			return render(request, 'amiispace_app/index.html', context)
 	else:
-		context['form'] = MyCardForm()
+		form = NewCardForm()
+		context['form'] = form
 	return render(request, 'amiispace_app/index.html', context)
